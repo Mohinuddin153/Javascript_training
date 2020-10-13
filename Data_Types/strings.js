84cd34118fb5dd3694417d06c0ffaff6ea111b6f
Mohinuddin153 <mohinuddinmoinu123@gmail.com>
2020-10-13 11:19:04 +0530

    updated

---
+++ Data_Types/strings.js
@@ -0,0 +1,16 @@
+let str = `Hello World`;
+console.log(str);
+
+//length of a string 
+console.log(str.length);
+
+//Accessing characters from string 
+
+console.log(str[2]);
+console.log(str.charAt(2));
+
+//Changing the case
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+console.log(str[2].toUpperCase())
+
