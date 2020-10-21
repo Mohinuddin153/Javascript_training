70c18e5f148444b2fc3815044745abd5cc6b0bc8
Mohinuddin153 <mohinuddinmoinu123@gmail.com>
2020-10-21 11:24:45 +0530

    custom time module

--- Analog_clock/modules/setTime.js
+++ Analog_clock/modules/setTime.js
@@ -2,8 +2,11 @@ import {clock,time,currentDate} from "../script.js"
 
 const h=document.getElementById("hour")
 const m=document.getElementById("minute")
-const b=document.getElementById("btn").onclick=rr
-function rr(){
+// h.value=currentDate.getHours();
+// m.value=currentDate.getMinutes()
+
+const b=document.getElementById("btn").onclick=setTime;
+function setTime(){
     currentDate.setHours(h.value,m.value,0)
     let obj2= new clock(currentDate);
     time(obj2)
